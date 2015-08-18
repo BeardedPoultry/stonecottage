79030a6dbad8ac9f6cf13c11f50948f49b18c09b
badview08 <jonesgabriel21@gmail.com>
2015-08-17 22:40:11 -0500

    added basic router tools

--- scripts/main.js
+++ scripts/main.js
@@ -4,4 +4,22 @@
   $(document).ready(function(){
     $('body').prepend(JST.application());
   });
+  
+  $(document).ready(function(){
+   window.stoneCottage = new stoneCottage();
+   Backbone.history.start();
+  });
+  $(document).on('click', '.thestonecottage', function(){
+    event.preventDefault();
+    Window.cottageRouter.navigate('/the-stone-cottage');
+  });
+  var cottageRouter = Backbone.Router.extend({
+    routes: {
+      '': 'index',
+      'theknittinggnome':'theknittinggnome',
+      'bythehearth':'bythehearth',
+      'intheherbgarden':'intheherbgarden',
+      'lifeatthecottage':'lifeatthecottage'
+    }
+  });
 })();
