739f2721f30c8ff20c8c9c1c3316a1ca9d1efeff
badview08 <jonesgabriel21@gmail.com>
2015-08-17 23:54:33 -0500

    added fontawesome and some stupid shit

--- scripts/main.js
+++ scripts/main.js
@@ -2,17 +2,19 @@
   'use strict';
 
   $(document).ready(function(){
-    $('body').prepend(JST.application());
+    $('body').append(JST.application());
   });
   
   $(document).ready(function(){
    window.stoneCottage = new stoneCottage();
    Backbone.history.start();
   });
+  
   $(document).on('click', '.thestonecottage', function(){
     event.preventDefault();
     Window.cottageRouter.navigate('/the-stone-cottage');
   });
+  
   var cottageRouter = Backbone.Router.extend({
     routes: {
       '': 'index',
@@ -20,6 +22,9 @@
       'bythehearth':'bythehearth',
       'intheherbgarden':'intheherbgarden',
       'lifeatthecottage':'lifeatthecottage'
-    }
+    },
+    // theknittinggnome: function(){
+    //   $('.theknittinggnome').html(JST[])
+    // }
   });
 })();
